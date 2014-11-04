bde7ebc212632f88a5b9af40d209bab09d418495
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-11-04 15:59:42 +0900

    Update header files

--- header.php
+++ header.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja" <?php language_attributes(); ?>>
   <head profile="http://gmpg.org/xfn/11">
-    <?php wp_head(); ?>
+    <meta <?php echo 'charset='. bloginfo('charset'); ?> />
+    <meta content="" name="keywords" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <?php if(is_category()): ?><?php elseif(is_archive()): ?>
+    <meta content="noindex,follow" name="robots" />
+    <?php elseif(is_tag()): ?>
+    <meta content="noindex,follow" name="robots" />
+    <?php endif; ?><?php wp_head(); ?>
   </head>
 </html>
