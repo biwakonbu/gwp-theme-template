b512570bb0d82ce02a4b0a72d73991aa5a35a603
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-11-04 16:34:00 +0900

    Update header files

--- header.php
+++ header.php
@@ -9,6 +9,29 @@
     <meta content="noindex,follow" name="robots" />
     <?php elseif(is_tag()): ?>
     <meta content="noindex,follow" name="robots" />
-    <?php endif; ?><?php wp_head(); ?>
+    <?php endif; ?><title><?php
+      global $page, $paged;
+      if(is_front_page()):
+        bloginfo('name');
+      elseif(is_single()):
+        wp_title('');
+      elseif(is_page()):
+        wp_title('');
+      elseif(is_archive()):
+        wp_title('|',true,'right');
+        bloginfo('name');
+      elseif(is_search()):
+        wp_title('-',true,'right');
+      elseif(is_404()):
+        echo'404 - ';
+        bloginfo('name');
+      endif;
+      if($paged >= 2 || $page >= 2):
+        echo'-'.sprintf('%sページ',
+        max($paged,$page));
+      endif;
+      ?></title>
+    <link <?php echo 'title='. bloginfo('name') .'RSS Feed'; echo 'href='. bloginfo('rss2_url'); ?>" rel="alternate" type="application/rss+xml />
+    <?php wp_head(); ?>
   </head>
 </html>
