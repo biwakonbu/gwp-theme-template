28abf88b1ccc87a642902b55462426740682d0eb
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-11-04 13:37:57 +0900

    Update watch rules

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var slim = require('gulp-slim');
+var shell = require('gulp-shell');
 var plumber = require('gulp-plumber');
 var compass = require('gulp-compass');
 
@@ -17,8 +18,22 @@ gulp.task('compass',function(){
         }));
 });
 
+gulp.task('php', ['slim'], function () {
+    return gulp.src('*.html', {read: false})
+        .pipe(shell([
+            "sed -e 's/\<!--//g' <%= file.path %> | sed -e 's/-->//g' > <%= f(file.path) %>",
+            'ls -l <%= f(file.path) %>'
+        ], {
+            templateData: {
+                f: function (s) {
+                    return s.replace(/\.html$/, '.php')
+                }
+            }
+        }))
+});
+
 gulp.task('slim', function(){
-    gulp.src(['slim/**/*.slim'])
+    return gulp.src(['slim/**/*.slim'])
         .pipe(plumber())
         .pipe(slim({pretty:true}))
         .pipe(gulp.dest('./'))
@@ -27,5 +42,11 @@ gulp.task('slim', function(){
 //watchのタスク
 gulp.task('watch',function(){
     gulp.watch('slim/**/*.slim', ['slim']);
+    gulp.watch('./**/*.html', ['php']);
     gulp.watch('sass/**/*.scss', ['compass']);
+});
+
+gulp.task('compile', function(){
+    gulp.run('php');
+    gulp.run('compass');
 });
